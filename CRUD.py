38a1937d61df9111d2af189e8264f166a62c3851
Rina kumari <Rina@gmail.com>
2025-02-09 19:11:15 +0530

    fetch all databases and diplay

--- CRUD.py
+++ CRUD.py
@@ -5,7 +5,45 @@ import mysql.connector as cn
 conn=cn.connect(
     host="localhost",
     user="root",
-    password="123456"
-    #database=""
+    password="123456",
+    database="novodaya_vidayalaya"
 )
+obj=conn.cursor()
 print("sucessfull connected" if conn.is_connected() else "Error")
+
+class CRUD_Operation:
+
+    #fetch all database 
+    def show_all_database(self):
+        query="SHOW DATABASES"
+        obj.execute(query)
+        val=obj.fetchall()
+        crud.show_database(val)
+    
+    #show the all databases
+    def show_database(self,val):
+        print("List of database: ")
+        print("-------------------")
+        lst=[]
+        for v in val:
+            for v1 in v:
+                lst.append(v1)
+        print(lst)
+    
+    
+    
+    
+crud=CRUD_Operation()
+#crud.show_all_database()
+    
+
+
+
+
+# def show_all_database():
+#     query="SHOW DATABASES"
+#     obj.execute(query)
+#     return obj.fetchall()
+# val=show_all_database()
+# print(val)
+
